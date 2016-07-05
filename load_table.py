3090bcc4739e3e11886b6260664ab767a492c703
Albert Han <hanalbert87@gmail.com>
2016-07-05 13:54:06 -0700

    misc

--- load_table.py
+++ load_table.py
@@ -32,20 +32,20 @@ with open('store_clean.h5', 'rb') as file:
     # Pickle the 'data' dictionary using the highest protocol available.
     clean_df = pd.read_hdf('store_clean.h5', 'table')
 
-print('---', type(clean_df))
-clean_dfT = clean_df.transpose()
-clean_dict = clean_dfT.to_dict()
-
-# print(type(clean_dict))
-# print(len(clean_dict))
-
-for_mongo_clean = []
-for key in clean_dict:
-    for_mongo_clean.append({key: clean_dict[key]})
-
-print(len(for_mongo_clean))
-print(type(for_mongo_clean))
-print(for_mongo_clean[100])
-
-with open('clean_collection.json', 'w') as outfile:  # do not write as binary
-    json.dump(for_mongo_clean, outfile, indent=2)
+# print('---', type(clean_df))
+# clean_dfT = clean_df.transpose()
+# clean_dict = clean_dfT.to_dict()
+#
+# # print(type(clean_dict))
+# # print(len(clean_dict))
+#
+# for_mongo_clean = []
+# for key in clean_dict:
+#     for_mongo_clean.append({key: clean_dict[key]})
+#
+# print(len(for_mongo_clean))
+# print(type(for_mongo_clean))
+# print(for_mongo_clean[100])
+#
+# with open('clean_collection_2.json', 'w') as outfile:  # do not write as binary
+#     json.dump(for_mongo_clean, outfile, indent=2)
